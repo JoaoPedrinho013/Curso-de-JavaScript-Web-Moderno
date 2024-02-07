88bdc436ed99234232122e5eb6e8e99e1f007af6
JoãoPedrinho013 <joaopedroliveira98@gmail.com>
2024-02-07 09:43:20 -0300

    objeto

---
+++ objeto/oo.js
@@ -0,0 +1,23 @@
+//  CODIGO NAO EXECUTAVEL!!!
+
+// PROCEDURAL 
+processamento(valor1, valor2, valor3)
+
+// OO
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // foco passou a ser o obejto
+
+// PRINCIPIOS IMPORTANTES: 
+// 1. Abstração
+// 2. Encapsulamento
+// 3. Herença (prototype)
+// 4. Poliformismo
