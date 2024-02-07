2fd076096e2b830c72fc30394b942497740f337f
JoãoPedrinho013 <joaopedroliveira98@gmail.com>
2024-02-07 07:32:33 -0300

    Update ex030.js

--- exerciciosGPT/ex030.js
+++ exerciciosGPT/ex030.js
@@ -1,1 +1,21 @@
 /**30 Crie uma função arrow que receba uma string e retorne a mesma string, mas com todas as letras maiúsculas transformadas em minúsculas e vice-versa. */
+
+
+
+
+const seForMaiuscRetornMinusc = string => {
+    let invertida = ""
+    for (let indice = 0; indice < string.length; indice++) {
+      let letra = string[indice]
+      if (letra === letra.toUpperCase()) {
+        invertida += letra.toLowerCase()
+      } else {
+        invertida += letra.toUpperCase()
+      }
+    }
+    console.log(invertida)
+
+}
+
+seForMaiuscRetornMinusc('Olá, Mundo!')
+seForMaiuscRetornMinusc('oLÁ, mUNDO!')
