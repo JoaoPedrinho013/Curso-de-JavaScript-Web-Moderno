5572c7ebec92927ee5416f32e3687b686289efbc
JoãoPedrinho013 <joaopedroliveira98@gmail.com>
2024-10-03 09:20:32 -0300

    express

---
+++ exercicios-express/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.all('/opa', (req, res)=> {
+    res.send('Estou <b>bem</b>!')
+})
+
+app.listen(3000, () => {
+    console.log('Backend rodando...')
+})
+
