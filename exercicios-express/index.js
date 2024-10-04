f9bbeb24f4ebabb9e017f67e52e3c55e5d51d5c1
JoãoPedrinho013 <joaopedroliveira98@gmail.com>
2024-10-04 09:15:41 -0300

    express

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,9 +1,62 @@
 const express = require('express')
 const app = express()
 
-app.all('/opa', (req, res)=> {
-    res.send('Estou <b>bem</b>!')
+const saudacao = require('./saudacao_mid')
+
+app.use(saudacao('Aziien'))
+
+app.use((req, res, next) => {
+    console.log('Antes...')
+    next()
 })
+
+app.get('/clientes/relatorio', (req, res) => {
+    res.send(`Cliente relatório: completo = ${req.query.completo} ano = ${req.query.ano}`)
+})
+
+app.post('/corpo', (req, res) => {
+    let corpo = ''
+    req.on('data', function(parte) {
+        corpo += parte
+    })
+
+    req.on('end', function(){
+        res.send(corpo)
+    })
+})
+
+app.get('/clientes/:id', (req, res) => {
+    res.send(`Cliente ${req.params.id} selecionado!`)
+})
+
+app.get('/opa', (req, res, next)=> {
+    console.log('Durante...')
+    res.json({
+        data: [
+            {id:7, name: 'Ana', position: 1},
+            {id:34, name: 'Bia', position: 2},
+            {id:73, name: 'Carlos', position: 3}
+        ],
+        count: 30,
+        skip: 0,
+        limit: 3,
+        status: 200
+    })
+    next()
+    //res.json({
+    //    name: 'iPad 32Gb',
+    //    price: 1899.00,
+    //    discount: 0.12
+    //})
+
+   //res.send('<h1>Estou bem!</h1> <br><br> <h2>Tipo HTML!</h2>')
+})
+
+app.use((req, res, next) => {
+    console.log('Depois...')
+    next()
+})
+
 
 app.listen(3000, () => {
     console.log('Backend rodando...')
